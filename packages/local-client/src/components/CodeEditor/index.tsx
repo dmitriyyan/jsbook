139e3698af4cfe50e54093c699bc166d0aee5517
dmitriyyan <dmitriy.ya@protonmail.com>
2023-04-17 16:44:01 +0300

    add local client

---
+++ packages/local-client/src/components/CodeEditor/index.tsx
@@ -0,0 +1,52 @@
+import { useCallback } from 'react';
+import { Editor, OnMount } from '@monaco-editor/react';
+
+import formatCode from './helpers/formatCode';
+import activateMonacoJSXHighlighter from './helpers/activateMonacoJSXHighlighter';
+import editorOptions from './helpers/editorOptions';
+
+type CodeEditorProps = {
+  input: string;
+  onChange: (value: string | undefined) => void;
+};
+
+const CodeEditor = ({ input, onChange }: CodeEditorProps) => {
+  const onFormatClick = () => {
+    onChange(formatCode(input));
+  };
+
+  const onMount = useCallback<OnMount>((editor, monaco) => {
+    activateMonacoJSXHighlighter(editor, monaco);
+  }, []);
+
+  return (
+    <div
+      className="is-relative"
+      style={{ width: 'calc(100% - 10px)', height: '100%' }}
+    >
+      <button
+        className="button button-format is-primary is-small"
+        style={{
+          position: 'absolute',
+          right: 15,
+          top: 7,
+          zIndex: 1,
+        }}
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
+      <Editor
+        value={input}
+        onChange={onChange}
+        height="100%"
+        defaultLanguage="javascript"
+        theme="vs-dark"
+        options={editorOptions}
+        onMount={onMount}
+      />
+    </div>
+  );
+};
+
+export default CodeEditor;
